f35aed80cf05050021490c8d3728652047bd3edc
Kunal Ghosh <kunalghosh02@outlook.com>
2017-12-15 13:00:32 +0530

    Create main.cpp
    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+
+void user( int user[5][5])
+{int i,j;
+ cout<<"\n Enter Your Bingo matrix Now:";
+ for(i=0;i<5;i++)
+   {cout<<"\n";
+    for(j=0;j<5;j++)
+    cout<<" ";
+    cin>>user[i][j];
+    }
+}
+
+void computer(int computer[5][5])
+{
+ 
+}
