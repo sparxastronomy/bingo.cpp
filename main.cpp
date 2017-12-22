64654df60f2e0c322bb664d0d5835a12d7476b04
Kunal Ghosh <kunalghosh02@outlook.com>
2017-12-22 21:51:27 +0530

    Update main.cpp
    this should work

--- main.cpp
+++ main.cpp
@@ -1,21 +1,70 @@
-#include<iostream.h>
+
+
+#include<iostream>
+#include<ctime>
+#include <windows.h> 
 #include<conio.h>
-#include<stdlib.h>
 
-void user( int user[5][5])
-{int i,j;
- cout<<"\n Enter Your Bingo matrix Now:";
- for(i=0;i<5;i++)
-   {cout<<"\n";
-    for(j=0;j<5;j++)
-    cout<<" ";
-    cin>>user[i][j];
-    }
-}
 
-void computer(int computer[5][5])
+using namespace std;
+
+void ClearScreen();
+
+int main()
 {
- for(i=0;i<5;i++)
-   for(j=0;j<5;j++)
-     computer[i][j]=random(
+
+	int num[5][5],number,row,col;
+	int ask;
+	bool ok = true;
+
+	srand((unsigned) time(0)); 
+
+	do{
+
+		 Clrscr(); 
+		cout<<"\n\n\t\t\t\t    BINGO GAME"<<endl;
+		for(row=0;row<5;row++)
+			for(col=0;col<5;col++)
+				num[row][col]=0; 
+		cout<<"\n\n\tB\t\tI\t\tN\t\tG\t\tO"<<endl;
+		cout<<"\n\n";
+		for(col = 0;col < 5; col++)
+		{
+			for (row=0;row < 5; row++)
+			{
+				do
+				{
+					ok = true;
+					number = 1+(col*15) + rand()%15;
+					for (int ck=0;ck<5;ck++)
+					{
+						if (number == num[ck][col])
+								ok = false; 
+					}
+				}while (!ok);
+			num[row][col] = number;
+			}
+		}
+
+		for(row = 0;row < 5; row++)
+		{
+			for (col=0;col < 5; col++)
+			{
+				if((row==2)&&(col==2))
+					cout<<"\tFREE\t";
+				else
+				cout << "\t" << num[row][col] << "\t";
+
+			}
+			cout << endl;
+		}
+
+		{
+			cout<<"\n\n\tIs this the card you want to use? [0 for no only.] : ";
+			cin>>ask;
+		}
+	} while(ask==0);
+
+	return 0;
 }
+
